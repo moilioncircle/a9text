9a8c7bf2c74abeea50c4969ee4da23cccc893fc1
Shi Rongjiu <trydofor@gmail.com>
2008-01-13 12:26:55 +0000

    java impl

---
+++ a9text/impl_java/src/com/trydofor/a9text/test/EclipseEncoding.java
@@ -0,0 +1,22 @@
+package com.trydofor.a9text.test;
+
+ /************************************************************
+ * @author  : Shi Rongjiu (www.trydofor.com)
+ * @version : 2008-1-13 / $Revision$
+ * @see     : requires/documents
+ * ====================== change-logs =======================
+ * $Log$
+ ************************************************************/
+
+public class EclipseEncoding {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
