c75dfa1962e504862ae9d93d63721e2d34a91a54
Shi Rongjiu <trydofor@gmail.com>
2008-01-14 04:29:16 +0000

    out

--- a9text/impl_java/src/com/trydofor/a9text/test/EclipseEncoding.java
+++ a9text/impl_java/src/com/trydofor/a9text/test/EclipseEncoding.java
@@ -1,5 +1,8 @@
 package com.trydofor.a9text.test;
 
+import java.io.File;
+import java.io.IOException;
+
  /************************************************************
  * @author  : Shi Rongjiu (www.trydofor.com)
  * @version : 2008-1-13 / $Revision$
@@ -10,13 +13,43 @@ package com.trydofor.a9text.test;
 
 public class EclipseEncoding {
 
+	
+	public void makePrefs(String root,String ext) throws IOException
+	{
+		File file = new File(root);
+		StringBuffer out = new StringBuffer();
+		walkDir(file,ext,out);
+		
+		System.out.println(out.toString());
+	}
+	
+	private void walkDir(File file,String ext,StringBuffer out) throws IOException
+	{
+		if(file.isDirectory())
+		{
+			File[] files = file.listFiles();
+			for (int i = 0; i < files.length; i++) 
+			{
+				walkDir(files[i],ext,out);
+			}
+		}
+		else
+		{
+			if(file.getName().endsWith(ext))
+			{
+				out.append(file.getCanonicalPath());
+				out.append("\n");
+			}
+		}
+	}
+	
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
-		// TODO Auto-generated method stub
-
+		EclipseEncoding ee = new EclipseEncoding();
+		ee.makePrefs("D:/workspace/a9text", ".js");
 	}
-
 }
