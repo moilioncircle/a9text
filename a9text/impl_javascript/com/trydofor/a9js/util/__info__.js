a050a429e2642e9ce002d70dfdec0646151d3a83
Shi Rongjiu <trydofor@gmail.com>
2008-02-23 11:32:35 +0000

    ho

--- a9text/impl_javascript/com/trydofor/a9js/util/__info__.js
+++ a9text/impl_javascript/com/trydofor/a9js/util/__info__.js
@@ -3,3 +3,5 @@
 __info__('task.js',['MiniTaskClass']);
 __info__('cnf.js',['CnfReaderClass']);
 __info__('xml.js',['XmlReaderClass']);
+__info__('log.js',['Logger']);
+__info__('trace.js',['Tracer']);
