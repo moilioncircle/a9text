b4b368dcda8ce3514e30d37aaf4abe651285d4d9
Shi Rongjiu <trydofor@gmail.com>
2008-02-25 03:08:06 +0000

    hah

---
+++ a9text/impl_javascript/a9loader.js
@@ -0,0 +1,42 @@
+﻿/**
+UTF8(BOM)  GPL  trydofor.com  Feb.2008
+===========================================================
+
+*/
+
+var __A9Loader__ = function()
+{
+    function __tagLoad__()
+    {
+        
+    }
+    
+    function __syncLoad__()
+    {
+        
+    }
+    
+    function __asyncLoad__()
+    {
+        
+    }
+    
+    function __init__()
+    {
+        
+    }
+    
+    //
+    this.tagLoad   = __tagLoad__;
+    this.syncLoad  = __syncLoad__;
+    this.asyncLoan = __asyncLoad__;
+    
+    // init
+    __init__();
+}
+
+// init instance
+if(!(A9Loader instanceof __A9Loader__))
+{
+    A9Loader    = new __A9Loader__();
+}
